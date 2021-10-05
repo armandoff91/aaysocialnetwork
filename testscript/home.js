bf181389c7a3034db48d98ee5751734b50b4c79e
armandoff91 <armandoff91@gmail.com>
2021-10-05 18:35:37 +0800

    copying to server

--- testscript/home.js
+++
@@ -1,6 +0,0 @@
-console.log("testscript/home.js loaded")
-var samplePost = {"date":1631194519146,"hidden":false,"lastUpdate":1631194519146,"__v":1.01,"_id":"613a0d97bd4dbb032efd8100","authorId":"60d9c658b1802301fe6a0061",
-"body":"Hello World 9 Sep","comments":[]}
-
-const commentDropdown = (event) => {
-}
